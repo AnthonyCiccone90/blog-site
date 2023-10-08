cc4d6901e2e53ff08b4c2c87d4fcf0bd50595b05
Anthony Ciccone <cicconeworldwide@gmail.com>
2023-10-08 13:30:35 -0600

    ?

--- controllers/signup-route.js
+++ controllers/signup-route.js
@@ -1,5 +1,37 @@
 const router = require('express').Router();
-const { User } = require('../models'); // Import your User model
+const bcrypt = rquite('bcrypt');
+const { User } = require('../models'); 
+
+const saltRounds = 10;
+const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+const newUser = await User.create({
+  username,
+  email,
+  password: hashedPassword,
+});
+
+router.post('/signup', async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+    const newUser = await User.create({ username, email password });
+    req.session.save(() => {
+      req.session.user_id = newUser.id;
+      req.session.logged_in = true;
+      res.status(200).json({ message: 'Signed up!' });
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Registration failed' });
+  }
+});
+
+
+req.session.save(() => {
+  req.session.user_id = newUser.id; // Save user ID to the session
+  req.session.logged_in = true; // Set the user as logged in
+  res.status(200).json({ message: 'Registration successful' });
+});
 
 
 module.exports = router;
--- server.js
+++ server.js
@@ -4,15 +4,20 @@ const session = require("express-session");
 const exphbs = require("express-handlebars");
 const routes = require("./controllers");
 const sequelize = require("./config/connection");
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const helpers = require("./utils/helpers");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sess = {
-  secret: "Super secret secret",
+  secret: 'your_secret_key', // Replace with a secure secret key
+  cookie: {},
   resave: false,
-  saveUninitialized: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
 };
 
 app.use(session(sess));

85695357889a8e0283bf521ffdd7c8a2a6f5e4ea
Anthony Ciccone <cicconeworldwide@gmail.com>
2023-10-10 18:08:38 -0600

    don't use this

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,12 +1,11 @@
 const isAuthenticated = (req, res, next) => {
-    if (req.session.logged_in) {
-      return next();
-    }
-  
-    res.redirect('/login'); // Redirect to the login page if not authenticated
-  };
-  
-  module.exports = {
-    isAuthenticated,
-  };
-  
+  if (req.session.logged_in) {
+    return next();
+  }
+
+  res.redirect('/login'); // Redirect to the login page if not authenticated
+};
+
+module.exports = {
+  isAuthenticated,
+};
